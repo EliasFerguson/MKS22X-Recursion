ba059c4b1867d4d016d8570086e3d546791a094c
Elias Ferguson <eferguson00@stuy.edu>
2019-01-31 12:23:52 -0500

    copy pasted working Newton's square root from other file

---
+++ recursion.java
@@ -0,0 +1,19 @@
+public class recursion {
+  public static void main(String[] args) {
+    System.out.println(sqrt(107));
+  }
+  public static double sqrt (double n) {
+    return sqrtH(n, n, .001);
+  }
+  public static double sqrtH(double n, double orig, double pc) {
+    if (sqrtCheck(n, orig, pc)) return n;
+    else {
+      return sqrtH(((orig / n + n) * .5), orig, pc);
+    }
+  }
+  public static boolean sqrtCheck(double n, double orig, double pc) {
+    double check = n * n;
+    if ((100 * Math.abs((check - orig) / orig)) < pc) return true;
+    return false;
+  }
+}
