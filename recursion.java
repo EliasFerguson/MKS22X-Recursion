5bd5888f5cb32210df78bb70464cc3ba9a664468
Elias Ferguson <eferguson00@stuy.edu>
2019-02-25 11:22:21 -0500

    added K's test cases, but unfortunately closeEnough() doesn't exist and i don't know what it does

--- recursion.java
+++ recursion.java
@@ -5,8 +5,8 @@ public class recursion {
     System.out.println(fib(6));
     System.out.println(makeAllSums(3).toString());
   }
-  public static double sqrt (double n) {
-    return sqrtH(n, n, .001);
+  public static double sqrt (double n, double pc) {
+    return sqrtH(n, n, pc);
   }
   public static double sqrtH(double n, double orig, double pc) {
     if (sqrtCheck(n, orig, pc)) return n;
@@ -40,4 +40,65 @@ public class recursion {
       makeAllSumsH(n - 1, sum, list);
     }
   }
+  //testcase must be a valid index of your input/output array
+public static void testFib(int testcase){
+  recursion r = new recursion();
+  int[] input = {0,1,2,3,5,30};
+  int[] output ={0,1,1,2,5,832040};
+  int max = input.length;
+  if(testcase < input.length){
+    int in = input[testcase];
+    try{
+
+      int ans = r.fib(in);
+      int correct = output[testcase];
+      if(ans == correct){
+        System.out.println("PASS test fib "+in+". "+correct);
+      }
+      else{
+        System.out.println("FAIL test fib"+in+". "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test fib"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
+
+
+//testcase must be a valid index of your input/output array
+public static void testSqrt(int testcase){
+  recursion r = new recursion();
+  double[] input = {0.0,1.0, 2.0, 4.0, 7.0};
+  double[] output = {0.0,1.0,1.4142135623730951,2.0,2.6457513110645907};
+  int max = input.length;
+  if(testcase < input.length){
+    double in = input[testcase];
+    try{
+      double ans = r.sqrt(in,.00001);
+      double correct = Math.sqrt(in);
+      if(closeEnough(ans,correct)){
+        System.out.println("PASS test sqrt "+in+" "+ans);
+      }
+      else{
+        System.out.println("FAIL test sqrt "+in+" "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test sqrt"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
 }
